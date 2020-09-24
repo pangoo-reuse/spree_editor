0b559ca20e4a92e91c570e981c977c40e6dac1df
pangoo <yonehsiung@gmail.com>
2020-09-24 22:42:05 +0800

    Update spree_editor.rb

--- lib/spree_editor.rb
+++ lib/spree_editor.rb
@@ -1,7 +1,7 @@
 require 'spree_core'
 require 'spree_editor/version'
 require 'spree_editor/engine'
-require 'coffee_script'
+#require 'coffee_script'
 require 'tinymce-rails'
 require 'ckeditor'
 require 'spree_extension'
